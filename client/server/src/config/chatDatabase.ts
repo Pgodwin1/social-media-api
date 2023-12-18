66c09544bcbcf9c634d3e26400a11bc8182db2ad
Pgodwin1 <pgodwin287@gmail.com>
2023-12-18 18:44:17 +0100

    updated

--- client/server/src/config/chatDatabase.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('connectify', '', '', {
-    storage: './connectify.sqlite',
-    dialect: 'sqlite',
-    logging: false
-})
-
-export default db;
